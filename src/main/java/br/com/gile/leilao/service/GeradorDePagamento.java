2aabaaab03bee1782b7c191e595ed25d0c557a71
Gileade <gileade.trindade@gmail.com>
2021-06-03 21:47:38 -0300

    Criando a classe GeradorDePagamentoTest, Utilizando Captor

--- src/main/java/br/com/gile/leilao/service/GeradorDePagamento.java
+++ src/main/java/br/com/gile/leilao/service/GeradorDePagamento.java
@@ -12,8 +12,12 @@ import br.com.gile.leilao.dao.PagamentoDao;
 @Service
 public class GeradorDePagamento {
 
-	@Autowired
 	private PagamentoDao pagamentos;
+
+	@Autowired
+	public GeradorDePagamento(PagamentoDao pagamentos) {
+		this.pagamentos = pagamentos;
+	}
 
 	public void gerarPagamento(Lance lanceVencedor) {
 		LocalDate vencimento = LocalDate.now().plusDays(1);
--- src/test/java/br/com/gile/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/gile/leilao/service/GeradorDePagamentoTest.java
@@ -1,4 +1,57 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.gile.leilao.service;
+
+import br.com.gile.leilao.dao.LeilaoDao;
+import br.com.gile.leilao.dao.PagamentoDao;
+import br.com.gile.leilao.model.Lance;
+import br.com.gile.leilao.model.Leilao;
+import br.com.gile.leilao.model.Pagamento;
+import br.com.gile.leilao.model.Usuario;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 class GeradorDePagamentoTest {
-  
+
+    private GeradorDePagamento gerador;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @BeforeEach
+    public void beforEach(){
+        MockitoAnnotations.initMocks(this);
+        this.gerador = new GeradorDePagamento(pagamentoDao);
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao(){
+        Leilao leilao = leilao();
+        Lance vencedor = leilao.getLanceVencedor();
+        gerador.gerarPagamento(vencedor);
+
+        //Captura um objeto de dentro do método testado
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+        Pagamento pagamento = captor.getValue();
+
+        Assert.assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+        Assert.assertEquals(vencedor.getValor(), pagamento.getValor());
+        Assert.assertFalse(pagamento.getPago());
+        Assert.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+        Assert.assertEquals(leilao, pagamento.getLeilao());
+    }
+
+    private Leilao leilao(){
+        Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Gile"));
+        Lance lance = new Lance(new Usuario("Ciclano"), new BigDecimal("900"));
+        leilao.propoe(lance);
+        leilao.setLanceVencedor(lance);
+        return leilao;
+    }
 }
